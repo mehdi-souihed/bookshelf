6fba93b8aa9ad555fdb6ca4f399a1115f50fac88
buddha <buddha@buddha.(none)>
2014-02-09 22:29:37 +0000

    adding dancer sessions

--- lib/Web.pm
+++ lib/Web.pm
@@ -10,71 +10,82 @@ use v5.10;
 our $VERSION = '0.1';
 
 get '/bookshelf' => sub {
-    template 'bookshelf_index';
+	
+	template 'bookshelf_index';
 };
 
 get '/add_book' => sub {
-    template 'add_book';
+	if(session('user')){
+
+	   template 'add_book';
+	}
+	else {
+	redirect '/bookshelf';
+	}
 };
 
 post '/add_book' => sub {
 
-my $book = Book->new(
-        title   => params->{title},
-        author  => params->{author},
-        pages   => params->{pages},
-        isbn    => params->{isbn},
-        year    => params->{year},
-        genre   => params->{genre},
-        url     => params->{url},
-        link_image => params->{link_image},
-        status  => params->{status},
-        tags    => params->{tags}, 
-);
+	my $book = Book->new(
+		title   => params->{title},
+		author  => params->{author},
+		pages   => params->{pages},
+		isbn    => params->{isbn},
+		year    => params->{year},
+		genre   => params->{genre},
+		url     => params->{url},
+		link_image => params->{link_image},
+		status  => params->{status},
+		tags    => params->{tags}, 
+	);
 
- Controller::add_book($book);
- 
- redirect '/bookshelf';
+	Controller::add_book($book);
+
+	redirect '/bookshelf';
 
 };
 
 get '/add_user' => sub {
-    template 'add_user';
+	template 'add_user';
 };
 
 post '/add_user' => sub {
 
-my $user = User->new(
-        fname   => params->{fname},
-        lname  => params->{lname},
-        email   => params->{email},
-        user_password    => params->{upass},
-);
+	my $user = User->new(
+		fname   => params->{fname},
+		lname  => params->{lname},
+		email   => params->{email},
+		user_password    => params->{upass},
+	);
 
- Controller::add_user($user);
- 
- redirect '/bookshelf';
+	Controller::add_user($user);
+
+	redirect '/bookshelf';
 
 };
 
 get '/login' => sub {
-    template 'login';
+	template 'login';
 };
 
 post '/login' => sub {
-
- Controller::login(params->{email}, params->{upass})? redirect '/bookshelf' : template 'login',{login_failed => 1};
-
+#	Validate the username and password 
+	if(Controller::login(params->{email}, params->{upass})){ 
+		session user => params->{email};
+		redirect '/bookshelf'; 	
+	}
+       	
+	template 'login',{login_failed => 1};
 };
 
 get '/search_book' => sub {
-    template 'search_book';
+	template 'search_book';
 };
 
 post '/results_books' => sub {
-    my $results = GoogleApi::search_book(params->{keyword});
-    
-    template 'results_books',{results => $results, search => params->{keyword}} ;
+	my $results = GoogleApi::search_book(params->{keyword});
+
+	template 'results_books',{results => $results, search => params->{keyword}} ;
 };
 
 
